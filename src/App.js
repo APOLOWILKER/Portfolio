2059b57f7053de722ea5165791395ee2d23acb54
APOLO WILKER <wilkerapolocarvalho@gmail.com>
2022-03-04 00:46:28 -0300

    update: refatorando o css

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
-      <Route path="/about" element={<Tecnologias />} />
+      <Route path="/tecnologie" element={<Tecnologias />} />
       <Route path="*" element={<NotFound/>} />
     </Routes>
     </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+// import Lottie from 'react-lottie';
 import '../styles/Header.css';
+// import animationData from './animation.json';
 
 function Header() {
+  // export const LikeButton = () => {
+  //   const [isLiked, setLikeState] = useState(false);
+  //   const [ animationState, setAnimationState ] = useState({
+  //     isStopped: true, isPaused: false
+  //   });
+  // https://www.youtube.com/watch?v=dZH9aDX8T-0&ab_channel=DevSoutinho
+  
+  //   const defaultOptions = {
+  //     loop: false,
+  //     autoplay: false, 
+  //     animationData: animationData,
+  //     rendererSettings: {
+  //       preserveAspectRatio: 'xMidYMid slice'
+  //     }
+  //   };
+  // };
+
+  
   return ( 
     <nav className='container-header'>
+      {/* <ButtonWrapper onClick={() => {
+          const reverseAnimation = -1;
+          const normalAnimation = 1;
+        setAnimationState({
+          ...animationState,
+          isStopped: false,
+          direction: animationState.direction === normalAnimation 
+            ? reverseAnimation
+            : normalAnimation,
+        });
+        setLikeState(!isLiked);
+      }}>
+        <Lottie options={defaultOptions}
+          height={400}
+          width={400}
+          isStopped={animationState.isStopped}
+          isPaused={setAnimationState.isPaused}/>
+      </ButtonWrapper>
+      <span>{isLiked ? 1 : 0}</span> */}
       <ul className='list-container-header'>
         <Link to="/"><li className='item-header'>HOME</li></Link>
         <Link to="/projects"><li className='item-header'>PROJETOS</li></Link>
-        <Link to="/about"><li className='item-header'>TECNOLOGIAS</li></Link>
+        <Link to="/tecnologie"><li className='item-header'>TECNOLOGIAS</li></Link>
       </ul>
     </nav>
   );
